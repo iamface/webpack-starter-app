85244fdcb10eec840ac662090e47d1590c3551ba
Jared Ivory <jared.ivory@zagg.com>
2017-02-23 09:59:01 -0700

    Added babili-webpack-plugin

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,18 +1,27 @@
 // Load strip loader
-var WebpackStripLoader = require('strip-loader');
+const WebpackStripLoader = require('strip-loader');
+
+// Babili plugin to compress production
+const BabiliPlugin = require('babili-webpack-plugin');
 
 // Include base config
-var devConfig = require('./webpack.config.js');
+const devConfig = require('./webpack.config.js');
 
 // Strip 'console.log' from files
-var stripLoader = {
+const stripLoader = {
     test: [/\.js$/],
     exclude: /node_modules/,
     loader: WebpackStripLoader.loader('console.log')
 };
 
+// Babili plugin
+let babili = new BabiliPlugin();
+
 // Include stripped files
 devConfig.module.loaders.push(stripLoader);
+
+// Include Babili plugin
+devConfig.plugins.push(babili);
 
 // Use base config
 module.exports = devConfig;
